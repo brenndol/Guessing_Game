c0a59c3705cd3f04ea85568f4b70f2f2f06c973f
Brenndol Magalhães <brendo1994@gmail.com>
2024-01-23 13:07:36 -0300

    Versão

---
+++ guessing/guessing/__main__.py
@@ -0,0 +1,18 @@
+from guessingGame import GuessingGame
+
+def main():
+    print("Welcome to the guessing game!")
+    print("First you will guess a number between 1 and 100.")
+    game = GuessingGame()
+    game.play()
+    game.changeGameKind()
+    game.play()
+    while True:
+        answer = input("Would you like to play again? (y/n) ")
+        if answer == 'y':
+            game.resetGame()
+            game.play()       
+    print("Thanks for playing!")
+
+if __name__ == '__main__':
+    main()
